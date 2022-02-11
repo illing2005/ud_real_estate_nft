a8a56f78f63e44d20c8d9800cfe28cf236696c77
Sebastian Illing <sebastian@alpaca.casa>
2022-02-11 21:41:35 +0100

    Implement SolnSquareVerifier contract

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,19 +1,83 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+import "./ERC721Mintable.sol";
 
-// TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is RealEstateERC721Token {
+    Verifier verifier;
 
-// TODO define a solutions struct that can hold an index & an address
+    // define a solutions struct that can hold an index & an address
+    struct Solution {
+        uint256 index;
+        address account;
+    }
 
-// TODO define an array of the above struct
+    // define an array of the above struct
+    Solution[] private solutions;
 
-// TODO define a mapping to store unique solutions submitted
+    // define a mapping to store unique solutions submitted
+    mapping(bytes32 => Solution) private uniqueSolutions;
 
-// TODO Create an event to emit when a solution is added
+    // Create an event to emit when a solution is added
+    event SolutionAdded(address account);
 
-// TODO Create a function to add the solutions to the array and emit the event
+    constructor(address verifierAddress) public {
+        verifier = Verifier(verifierAddress);
+    }
 
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSupply
+    // Create a function to add the solutions to the array and emit the event
+    function addSolution(bytes32 solutionKey, uint256 tokenId) public {
+        Solution memory solution = Solution({
+            index: tokenId,
+            account: msg.sender
+        });
+        solutions.push(solution);
+        uniqueSolutions[solutionKey] = solution;
+        emit SolutionAdded(msg.sender);
+    }
+
+    // Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSupply
+    function mintNFT(
+        address to,
+        uint256 tokenId,
+        uint256[2] memory a,
+        uint256[2] memory a_p,
+        uint256[2][2] memory b,
+        uint256[2] memory b_p,
+        uint256[2] memory c,
+        uint256[2] memory c_p,
+        uint256[2] memory h,
+        uint256[2] memory k,
+        uint256[2] memory input
+    ) public {
+        bytes32 solutionKey = keccak256(
+            abi.encodePacked(a, a_p, b, b_p, c, c_p, h, k, input)
+        );
+        require(
+            uniqueSolutions[solutionKey].account == address(0),
+            "Solution has already been used"
+        );
+        require(
+            verifier.verifyTx(a, a_p, b, b_p, c, c_p, h, k, input),
+            "Solution could not be verified"
+        );
+        addSolution(solutionKey, tokenId);
+        // finally mint the token
+        mint(to, tokenId);
+    }
+}
+
+contract Verifier {
+    function verifyTx(
+        uint256[2] memory a,
+        uint256[2] memory a_p,
+        uint256[2][2] memory b,
+        uint256[2] memory b_p,
+        uint256[2] memory c,
+        uint256[2] memory c_p,
+        uint256[2] memory h,
+        uint256[2] memory k,
+        uint256[2] memory input
+    ) public returns (bool r);
+}
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,62 @@
 // Test if a new solution can be added for contract - SolnSquareVerifier
-
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+
+var SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
+var Verifier = artifacts.require("Verifier");
+
+contract("TestSolnSquareVerifier", (accounts) => {
+  const account_one = accounts[0];
+  const account_two = accounts[1];
+  const account_three = accounts[2];
+
+  describe("Mint verified NFT", function () {
+    beforeEach(async function () {
+      this.verifier = await Verifier.new({
+        from: account_one,
+      });
+      this.contract = await SolnSquareVerifier.new(this.verifier.address, {
+        from: account_one,
+      });
+    });
+
+    it("should verify and mint a token", async function () {
+      const { proof, input } = require("../../zokrates/code/square/proof.json");
+      await this.contract.mintNFT(
+        account_two,
+        1,
+        proof.A,
+        proof.A_p,
+        proof.B,
+        proof.B_p,
+        proof.C,
+        proof.C_p,
+        proof.H,
+        proof.K,
+        input
+      );
+      const accountTwoBalance = await this.contract.balanceOf(account_two);
+      assert.equal(accountTwoBalance, 1, "Account should have a token minted");
+
+      // Minting with same solution should fail
+      let reverted = false;
+      try {
+        await this.contract.mintNFT(
+          account_two,
+          2,
+          proof.A,
+          proof.A_p,
+          proof.B,
+          proof.B_p,
+          proof.C,
+          proof.C_p,
+          proof.H,
+          proof.K,
+          input
+        );
+      } catch (e) {
+        reverted = true;
+      }
+      assert.equal(reverted, true);
+    });
+  });
+});
